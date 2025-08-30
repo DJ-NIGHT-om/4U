2c92c53a62ca3b2c534776008b31cc510d3e2700
DJ-NIGHT-om <alndiri2@gmail.com>
2025-08-30 08:09:23 +0400

    Update sync.js

--- sync.js
+++ sync.js
@@ -3,7 +3,7 @@
     'use strict';
     
     /* @tweakable The delay in milliseconds between each background data sync. 120000ms = 3 minutes. */
-    const syncFrequency = 12000;
+    const syncFrequency = 11000;
     var syncInterval;
     var allPlaylists = [];
     var allSheetData = []; // To store all data from the sheet for date checking
